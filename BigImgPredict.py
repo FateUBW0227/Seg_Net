95aa01d67a111c8ed87f479bfc3f799583f0cb56
FateUBW0227 <FateUBW@mail.ustc.edu.cn>
2024-05-21 16:53:11 +0800

    add files

--- BigImgPredict.py
+++ BigImgPredict.py
@@ -3,76 +3,66 @@ import json
 import os
 import shutil
 
+
 os.environ['KMP_DUPLICATE_LIB_OK']='True'
 import time, torch
 import numpy as np
 import tifffile, os
 from os.path import join
 from ModelPredictPy import ModelPredictClass
+# from skimage import exposure
 
 if __name__ == '__main__':
-    imgPath = r"D:\qxz\MyProject\KKMarkCellBody\Code\BrainSegNewQxz\SegmentProblem\DataSet\img"
-    modelPath = r'./ModelSave/exp006/supernet_00010.pth'
-    # testTxt = r'test.txt'
-    savePath = r'D:\qxz\MyProject\KKMarkCellBody\Code\BrainSegNewQxz\SegmentProblem\DataSet\mask'
+ #   imgPath = r'/media/MD3400-2/Cailin/test_data'
+ #   savePath = r'/media/MD3400-2/Cailin/training_data/predict_result_exp025_64pth'
+    imgPath = r'./test_data'
+    savePath = r'./save_data'
+
+    modelPath = r'./ModelSave/exp025/supernet_00030.pth'
     bigImgSize = np.array([512, 512, 512], dtype=np.int32)
     imgSize = np.array([128, 128, 128], dtype=np.int32)
+    r = 32          # 冗余
+    rSp = 16        # 边缘不要部分
     batchSize = 1
     fieldLen = 16
-    device = torch.device('cuda:0')
+    device = torch.device('cuda:1')
 
-    # cfgPath = join(savePath, 'info.json')
-    # saveRes = join(savePath, 'result')
-
-    # if os.path.isdir(savePath): shutil.rmtree(savePath)
-    # os.makedirs(savePath)
-    # if os.path.isdir(saveRes): shutil.rmtree(saveRes)
-    # os.makedirs(saveRes)
-
-    saveRes = savePath
-    os.makedirs(saveRes, exist_ok=True)
-
+    os.makedirs(savePath, exist_ok=True)
     model = ModelPredictClass(modelPath, fieldLen=fieldLen, device=device)  # 预测类
-    # with open(join(rootPath, testTxt), 'r') as f:
-    #     ls = f.read().strip().split('\n')
-    # imgPath = join(rootPath, 'images')
+    # # ls = [os.listdir(imgPath)[220]]
+    # ls = ['10623_8104_831.tif']
     ls = os.listdir(imgPath)
-
     saveInfo = []
-    for name in ls:
+    for ii, name in enumerate(ls):
+        s1 = time.time()
         oriImg = tifffile.imread(join(imgPath, name))
+        # oriImg = (oriImg - oriImg.min()) / (oriImg.max() - oriImg.min())
+        # oriImg = (exposure.equalize_hist(oriImg) * 2700).astype(np.uint16)
+        # tifffile.imwrite(r'D:\qxz\MyProject\KKMarkCellBody\Code\NeuronTrack\DataSet\ProblemDataSet\SegProblemTest\SmallImages\ttt.tif', oriImg)
+        # oriImg[oriImg > 125] = 125
+        # oriImg
         if oriImg.ndim != 3: continue
-        # oriImg_eq = exposure.equalize_hist(oriImg, nbins=oriImg.max() - oriImg.min())
-        # newImg = np.zeros_like(oriImg)
         maskBigImg = np.zeros(bigImgSize[::-1], dtype=np.uint8)
-        sliceNumber = bigImgSize // imgSize
-        count = 0
+        sliceNumber = np.ceil((bigImgSize - imgSize) / (imgSize - r)).astype(np.int32) + 1
         newName = os.path.splitext(name)[0]
         for nz in range(sliceNumber[2]):
             for ny in range(sliceNumber[1]):
                 for nx in range(sliceNumber[0]):
-                    s1 = time.time()
-                    sp = imgSize * [nx, ny, nz]
-                    ep = sp + imgSize
+                    sp = (imgSize - r) * [nx, ny, nz]
+                    ep = np.min([sp + imgSize, bigImgSize], axis=0)
+                    sp = np.min([sp, ep - imgSize], axis=0)
                     img = oriImg[sp[2]: ep[2], sp[1]: ep[1], sp[0]: ep[0]]
-                    # img_eq = oriImg_eq[sp[2]: ep[2], sp[1]: ep[1], sp[0]: ep[0]]
-                    # img = np.array([img, img_eq], dtype=np.float32)
-
-                    # newImg[sp[2]: ep[2], sp[1]: ep[1], sp[0]: ep[0]] = img
+                    # stdVal = img.std()
+                    # if stdVal > 500:
+                    #     print('**********************')
+                    # print(name, nx, ny, nz, img.std())
                     mask = model(img)
-                    maskBigImg[sp[2]: ep[2], sp[1]: ep[1], sp[0]: ep[0]] = mask
-                    print(count, time.time() - s1)
-                    count += 1
-                    # tifffile.imwrite(join(savePath, '%s_%s_0.tif' % (newName, str(count).zfill(5))), img)
-                    # tifffile.imwrite(join(savePath, '%s_%s_1.tif' % (newName, str(count).zfill(5))), mask)
-                    # print('%s_%s_1.tif' % (newName, str(count).zfill(5)), mask.sum())
-
-                    # break
-                # break
-            # break
-        # tifffile.imwrite(join(savePath, '%s_0.tif' % newName), oriImg[:bigImgSize[2], :bigImgSize[1], :bigImgSize[0]])
+                    rsp2 = rSp * np.sign([nx, ny, nz])
+                    sp += rsp2
+                    maskBigImg[sp[2]: ep[2], sp[1]: ep[1], sp[0]: ep[0]] = mask[rsp2[2]:, rsp2[1]:, rsp2[0]:]
         maskBigImg[maskBigImg < 103] = 0
-        tifffile.imwrite(join(saveRes, '%s.tif' % newName), maskBigImg)
+        tifffile.imwrite(join(savePath, '%s.tif' % newName), maskBigImg)
         # saveInfo.append([join(saveRes, '%s.tif' % newName), join(imgPath, name), ''])
         # with open(cfgPath, 'w') as f:
         #     f.write(json.dumps(saveInfo))
+        print(ii, len(ls), time.time() - s1)
--- README.md
+++ README.md
@@ -1,2 +1,44 @@
-# Seg_Net
- 
+## Prerequisites
+
+1. Install required library
+
+   ```python
+   pip install -r requirements.txt
+   ```
+
+   
+
+2. Install pytorch-3dunet from https://github.com/wolny/pytorch-3dunet
+
+
+
+## Train
+
+- Data Preprocess
+
+
+
+
+
+- Train Segment model with command
+
+  ``` python
+  python Train.py
+  ```
+
+  
+
+## Run
+
+
+
+
+
+
+
+
+
+
+
+
+
